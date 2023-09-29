5b7a4b201f49875116b80349c058fd5a20d5e26e
Sanjeev Devarajan <realsanjeevd@gmail.com>
2023-09-29 17:45:08 -0400

    working on addEvent

--- frontend-folder/code-for-good/src/App.js
+++ frontend-folder/code-for-good/src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
-// import Home from './pages/Home'
 import Decision from './pages/Decision'
-
 import Header from './components/Header'
 import React from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
