0b5355236f7450ca9cf9839fd719ef18acd1ef9a
pascalbell <pascalbell16@gmail.com>
2023-09-29 16:28:11 -0400

    bare bones

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+//import express from "express";
+
+const bodyParser = require("body-parser");
+const express = require("express");
+const PORT = 80;
+const app = express();
+
+/*mongoose.connect(process.env.MONGODB_URI!)
+    .then(() => { console.log("connected") })
+    .catch((err) => { console.log(err) });*/
+
+app.use('/api', router2);
+app.use('/api', router);
+
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
