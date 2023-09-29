d8764182a26d7fdd0dbf96adadadb7aeefe21773
akshatchavann <akshatchavan2022@gmail.com>
2023-09-29 16:41:36 -0400

    mongodb access

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,21 @@
-//import express from "express";
-
 const bodyParser = require("body-parser");
 const express = require("express");
 const PORT = 80;
 const app = express();
+const mongoose = require("mongoose");
 
-/*mongoose.connect(process.env.MONGODB_URI!)
-    .then(() => { console.log("connected") })
-    .catch((err) => { console.log(err) });*/
 
+app.use(bodyParser.json());
 app.use('/api', router2);
 app.use('/api', router);
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(()=>{
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
