0dbb228149bfe25da2b43df0cc3e1d999fc9ae31
pascalbell <pascalbell16@gmail.com>
2023-09-29 16:45:53 -0400

    testing

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,9 @@ const express = require("express");
 const PORT = 80;
 const app = express();
 
-/*mongoose.connect(process.env.MONGODB_URI!)
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => { console.log("connected") })
-    .catch((err) => { console.log(err) });*/
+    .catch((err) => { console.log(err) });
 
 app.use('/api', router2);
 app.use('/api', router);
