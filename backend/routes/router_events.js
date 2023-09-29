73f81c5c422051b2945a907313f6c25e0396a21c
pascalbell <pascalbell16@gmail.com>
2023-09-29 17:33:52 -0400

    working on post events

--- backend/routes/router_events.js
+++ backend/routes/router_events.js
@@ -1,11 +1,21 @@
 import {Request, Response, Router} from "express";
-import getPostModel from "../schema/Event";
+import EventSchema from "../schema/Event";
 
 const events = Router();
 
 events.post("/create-event", async (Request, Response) => {
-    const { id, title, date, recurring, category, zoomLink } = req.body;
-    const eventDB = await UserModel.findOne({ id: user._id });
+    const { id, title, date, recurring, category, zoomLink } = Request.body;
+    const eventDB = await EventSchema.findOne({ id: id });
+
+    if (eventDB) {
+        Response.status(400).send("Event already exists!");
+        return;
+    }
+
+    const event = EventSchema.create({
+
+    });
+    event.save();
 })
 
 export default events;
