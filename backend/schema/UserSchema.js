2b4e23d563153210c5cfc391d012b6bb6894966c
Harbin Hong <hhong61502@gmail.com>
2023-09-29 18:57:48 -0400

    add phoen number field to user schema

--- backend/schema/UserSchema.js
+++ backend/schema/UserSchema.js
@@ -7,6 +7,7 @@ const userSchema = new Schema({
     lastname: { type: String, required: true},
     email: { type: String, required: true},
     password: { type: String, required: true},
+    phoneNumber: { type: String, required: true},
     interests: { type: String, required: true}, //double check if this is an array or string
 });
 
