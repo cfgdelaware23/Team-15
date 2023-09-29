e41553897353f7d147aedda2738e4216301bcbb3
shubhiupa19 <s.x.upadhyay@gmail.com>
2023-09-29 17:28:06 -0400

    Update Decision.js

--- frontend-folder/code-for-good/src/pages/Decision.js
+++ frontend-folder/code-for-good/src/pages/Decision.js
@@ -2,7 +2,11 @@ import React from 'react'
 import styled from 'styled-components';
 
 const Button = styled.button`
+<<<<<<< Updated upstream
   background-color: black;
+=======
+  background-color: green;
+>>>>>>> Stashed changes
   color: white;
   font-size: 20px;
   padding: 10px 60px;
@@ -10,7 +14,11 @@ const Button = styled.button`
 `;
 
 
+<<<<<<< Updated upstream
 function Decision() {
+=======
+function Decision() {br
+>>>>>>> Stashed changes
   return (
     <div>Decision
       <Button>
