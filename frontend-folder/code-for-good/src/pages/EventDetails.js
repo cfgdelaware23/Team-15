abd4bdd2b2b885d7202a56c8824a2cb7f48b1ccc
Sanjeev Devarajan <realsanjeevd@gmail.com>
2023-09-30 05:00:02 -0400

    can join event details

--- frontend-folder/code-for-good/src/pages/EventDetails.js
+++ frontend-folder/code-for-good/src/pages/EventDetails.js
@@ -1,13 +1,65 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { db } from "../firebase-config.js";
-import { collection, getDocs } from "firebase/firestore";
+import { doc, collection, getDocs, updateDoc } from "firebase/firestore";
 
 const EventDetails = () => {
-  const { eventTitle } = useParams();
+  const { eventTitle, userId } = useParams();
 
   const [eventData, setEventData] = useState(null); // Use null instead of []
   const [loading, setLoading] = useState(true);
+  
+  const addUser = async () => {
+    try {
+      const userDocRef = collection(db, "users");
+      const userDocSnapshot = await getDocs(userDocRef);
+        
+      let isAdmin = false;
+  
+      userDocSnapshot.forEach((doc) => {
+        const userData = doc.data();
+        if (userData.admin === true) {
+          isAdmin = true;
+        }
+      });
+  
+      const eventDocRef = collection(db, "events");
+      const eventDocSnapshot = await getDocs(eventDocRef);
+  
+      eventDocSnapshot.forEach((doc1) => {
+        const documentData = doc1.data();
+        if (documentData.title === eventTitle) {
+          // Ensure volunteers and attendees are initialized as arrays
+          documentData.volunteers = documentData.volunteers || [];
+          documentData.attendees = documentData.attendees || [];
+  
+          if (isAdmin === true) {
+            if (!documentData.volunteers.includes(userId)){
+              documentData.volunteers.push(userId);
+
+            }
+          } else {
+            if (!documentData.attendees.includes(userId)){
+              documentData.attendees.push(userId);
+            }
+          }
+
+          documentData.volunteers = documentData.volunteers || [];
+          documentData.attendees = documentData.attendees || [];
+  
+
+          const docRef = doc(db, "events", doc1.id);
+          updateDoc(docRef, {
+            attendees: documentData.attendees,
+            volunteers: documentData.volunteers,
+          });
+        }
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  
 
   useEffect(() => {
     // Fetch event data from Firestore inside a useEffect hook
@@ -52,6 +104,7 @@ const EventDetails = () => {
         <p>This event occurs every {eventData.recurringDays} days</p>
         <p>Categories: {eventData.interests.join(", ")}</p>
         <p>Zoom Link: {eventData.zoom}</p>
+        <button onClick={addUser}>Add Event</button>
       </article>
     </div>
   );
