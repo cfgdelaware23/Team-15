a37f525f66821d9527255a2212fb11e2754f313b
Harbin Hong <hhong61502@gmail.com>
2023-09-29 22:22:51 -0400

    adding back routes to app.js

--- frontend-folder/code-for-good/src/App.js
+++ frontend-folder/code-for-good/src/App.js
@@ -8,13 +8,13 @@ import AddEvent from './pages/AddEvent'
 import JoinEvent from './pages/JoinEvent'
 import UserSignup from './pages/UserSignup'
 import Description from './pages/Description';
+import EventDashboard from "./pages/EventDashboard";
+import EventDetails from "./pages/EventDetails";
+
 
 function App() {
   return (
     <div className="App">
-       
-     
-  
       <Header />
       <BrowserRouter>
         <Routes>
@@ -25,10 +25,10 @@ function App() {
           <Route path="/Decision" element={<Decision />} />
           <Route path="/AddEvent" element={<AddEvent />} />
           <Route path="/JoinEvent" element={<JoinEvent />} />
-            
+          <Route path="/EventDashboard" element={<EventDashboard />} />
+          <Route path="/events/:id" element={<EventDetails />} />
         </Routes>
       </BrowserRouter>
-
     </div>
   );
 }
