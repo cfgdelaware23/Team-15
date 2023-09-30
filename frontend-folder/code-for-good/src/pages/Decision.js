5e176c876d8f57f1c7f74ce4d199c7608ba83eca
Sanjeev Devarajan <realsanjeevd@gmail.com>
2023-09-30 05:07:11 -0400

    asd

--- frontend-folder/code-for-good/src/pages/Decision.js
+++ frontend-folder/code-for-good/src/pages/Decision.js
@@ -3,28 +3,7 @@ import '../styles/Decision.css'
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-const Button = styled.button`
-background-color: #93C5CD;
-color: #fff;
-font-size: 18px;
-font-family: 'Poppins', sans-serif;
-font-weight: 600;
-border: none;
-border-radius: 4px;
-cursor: pointer;
-padding: 10px 60px;
-border-radius: 5px;
-margin: 10px;
-
-&:hover {
-  background-color: darkblue;
- 
-}
-
-&:hover {
-  background-color: darkblue;
-}
-`;
+const Button = styled.button;
 
 
 function Decision() {
