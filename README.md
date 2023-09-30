fd82ec20599812128edbec807d636171b264f98c
akshatchavann <akshatchavan2022@gmail.com>
2023-09-30 02:48:46 -0400

    routing/backend

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Team-15
+
+
+This application is a product of the powerful MERN stack, meticulously designed to cater to the needs of the American Association of the Blind. At its core, our software provides an intuitive platform for seamless management and coordination of events. Specifically tailored for the administrators and volunteers of the association, our platform aims to simplify their tasks and enhance efficiency, ensuring that their invaluable efforts in assisting the visually impaired community remain unhindered.
+ 
--- backend/routes/router_users.js
+++ backend/routes/router_users.js
@@ -1,3 +1,4 @@
+
 // const express = require('express');
 // const HttpError = require('../models/http-error')
 // const Usercontrollers = require('../controllers/Usercontrollers')
@@ -8,35 +9,24 @@
 
 // module.exports = userrouter;
 
-const router = require("express").Router();
-let User = require("../schema/UserSchema");
+const router = require('express').Router();
+let User = require('../schema/UserSchema');
 
-router.route("/").get((req, res) => {
-  User.find()
-    .then((users) => res.json(users))
-    .catch((err) => res.status(400).json("Error: " + err));
+router.route('/').get((req, res) => {
+    User.find()
+        .then(users => res.json(users))
+        .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route("/add").post((req, res) => {
-  const firstname = req.body.firstname;
-  const lastname = req.body.lastname;
-  const email = req.body.email;
-  const phoneNumber = req.body.phoneNumber;
-  const password = req.body.password;
-  const interests = req.body.interests;
+router.route('/add').post((req, res) => {
+    const username = req.body.username;
 
-  const newUser = new User({ 
-    firstname,
-    lastname,
-    email,
-    phoneNumber,
-    password,
-    interests
-  });
+    const newUser = new User({username});
 
-  newUser.save()
-    .then(() => res.json("User added!"))
-    .catch((err) => res.status(400).json("Error: " + err));
+    newUser.save()
+        .then(() => res.json("User added!"))
+        .catch(err => res.status(400).json('Error: ' + err))
 });
+
 
 module.exports = router;
