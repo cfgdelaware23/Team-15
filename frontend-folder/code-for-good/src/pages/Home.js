140c447303390a2f5e80fd676cfd08c5c6f94bb5
shubhiupa19 <s.x.upadhyay@gmail.com>
2023-09-29 20:07:44 -0400

    Update Home.js

--- frontend-folder/code-for-good/src/pages/Home.js
+++ frontend-folder/code-for-good/src/pages/Home.js
@@ -8,7 +8,7 @@ import Signup from '../components/Signup'
 function Home() {
   return (
 
-    <div className={styles.pageLayout}>
+    <div className={'pageLayout'}>
       <h1>Home Page</h1>
       <p>
         Our project aims to help ACB expand and maintain their virtual volunteer-led programming, while maintaining accessibility for the blind and low vision community.
