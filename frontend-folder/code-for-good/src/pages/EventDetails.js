d24f88099d4d2318cfc298cfb8a2dac440f7979c
Harbin Hong <hhong61502@gmail.com>
2023-09-30 00:06:45 -0400

    harbin's commit, eventdashboard and eventdetails now pulling data from firebase

--- frontend-folder/code-for-good/src/pages/EventDetails.js
+++ frontend-folder/code-for-good/src/pages/EventDetails.js
@@ -1,53 +1,60 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { db } from "../firebase-config.js";
+import { doc, collection, getDocs } from "firebase/firestore";
 
 const EventDetails = () => {
+  const { id } = useParams();
+  const eventId = parseInt(id, 10);
+  const [eventData, setEventData] = useState(null); // Use null instead of []
+  const [loading, setLoading] = useState(true);
 
-    const events = [
-      {
-        id: 1,
-        title: "Shuffleboard",
-        date: "October 26, 2022",
-        recurring: 7,
-        category: ["Entertainment", "Sports"],
-        zoomLink: "https://al;aejfonajkl;la.com",
-      },
-      {
-        id: 2,
-        title: "basketball",
-        date: "October 21, 2023",
-        recurring: 14,
-        category: ["Education"],
-        zoomLink: "https://al;aejf.com",
-      },
-      {
-        id: 3,
-        title: "movie",
-        date: "December 5, 2021",
-        recurring: 7,
-        category: ["Sports"],
-        zoomLink: "https://al;aejfonajhfadehj;fa.com",
-      },
-    ];
-    
-    const {id} = useParams()
+  useEffect(() => {
+    // Fetch event data from Firestore inside a useEffect hook
+    const fetchData = async () => {
+      try {
+        const eventDocRef = collection(db, "events");
+        const eventDocSnapshot = await getDocs(eventDocRef);
 
-    const eventId = parseInt(id, 10);
+        eventDocSnapshot.forEach((doc) => {
+            const documentData = doc.data();
 
-    const thisEvent = events.find((item) => item.id === eventId);
+            if (documentData.id === eventId){
+                setEventData(documentData)
+            }
+        })
 
-    return (
-        <div className="eventDetails">
-            <article>
-                <h2>{thisEvent.title}</h2>
-                <p>Date is on {thisEvent.date} </p>
-                <p>This event occurs every {thisEvent.recurring} days </p>
-                <p>Categories: {thisEvent.category}</p>
-                <p>Zoom Link: {thisEvent.zoomLink}</p>
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false); // Set loading to false regardless of success or error
+      }
+    };
 
-            </article>
-        </div>
-    );
-    
-}
+    fetchData();
+  }, [eventId]); // Pass [eventId] as a dependency to re-run the effect when the ID changes
+
+  if (loading) {
+    // Render a loading state while data is being fetched
+    return <div>Loading...</div>;
+  }
+
+  if (!eventData) {
+    // Render an error message if event data is not found
+    return <div>Event not found</div>;
+  }
+
+  return (
+    <div className="eventDetails">
+      <article>
+        <h2>{eventData.title}</h2>
+        <p>Date is on {eventData.date}</p>
+        <p>This event occurs every {eventData.recurring} days</p>
+        <p>Categories: {eventData.category.join(", ")}</p>
+        <p>Zoom Link: {eventData.zoomLink}</p>
+      </article>
+    </div>
+  );
+};
 
 export default EventDetails;
